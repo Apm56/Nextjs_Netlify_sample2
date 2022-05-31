9778ee24ae387d012edb612afc1ee64fc4b208fc
APM <>
2022-05-31 16:36:56 +0530

    Add the files with proper netlify url

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ module.exports = nextConfig
 
 module.exports = {
   env: {
-      MONGO_URI:  "mongodb+srv://stdb:st123@cluster0.nnibg.mongodb.net/nextSample_db?retryWrites=true&w=majority"
+      MONGO_URI:  "mongodb+srv://stdb:st123@cluster0.nnibg.mongodb.net/nextSample_db?retryWrites=true&w=majority",
+
+      BASE_URL: "https://nextjs-netlify-sample.netlify.app/",
+
+      API_URL: "https://nextjs-netlify-sample.netlify.app/api/",
+
+      LOCAL_URL: "http://localhost:3000/",
+
+
   }
 }
--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -21,7 +21,7 @@ const Note = ({ note }) => {
     const deleteNote = async () => {
         const noteId = router.query.id;
         try {
-            const deleted = await fetch(`http://localhost:3000/api/notes/${noteId}`, {
+            const deleted = await fetch(`BASE_URL/api/notes/${noteId}`, {
                 method: "Delete"
             });
 
@@ -57,7 +57,7 @@ const Note = ({ note }) => {
 }
 
 Note.getInitialProps = async ({ query: { id } }) => {
-    const res = await fetch(`http://localhost:3000/api/notes/${id}`);
+    const res = await fetch(`BASE_URL/api/notes/${id}`);
     const { data } = await res.json();
 
     return { note: data }
